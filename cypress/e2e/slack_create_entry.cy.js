ae49bd24ce71bf225e93599e99998e2e8bd596c4
Amazia Gur <amaziagur@Amazias-MacBook-Pro.local>
2025-01-26 13:41:45 +0000

    fix: append timestamp to entry

--- cypress/e2e/slack_create_entry.cy.js
+++ cypress/e2e/slack_create_entry.cy.js
@@ -4,7 +4,7 @@ describe('Add slack entry and verify on BusyBe website', () => {
   it('should add entry via slack and verify on BusyBe website', () => {
     const slackApiUrl = "https://clear-slate-8b4de92f5776.herokuapp.com/api/slack/message-shortcut/"
     const slackToken = Cypress.env('SLACK_TOKEN');
-    const testMessage = "E2E Rocks!"
+    const testMessage = `E2E Rocks! ${new Date().toISOString()}`
 
     if (!slackToken) {
       throw new Error("SLACK_TOKEN is not defined. Please set it before running the test.");
