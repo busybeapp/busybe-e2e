4aae4821a498aae50cd62d12eab9c46380379ed4
Amazia Gur <amaziagur@Amazias-MacBook-Pro.local>
2025-01-27 14:05:35 +0000

    refactor: encapsulate data from test

--- cypress/e2e/slack_create_entry.cy.js
+++ cypress/e2e/slack_create_entry.cy.js
@@ -1,25 +1,16 @@
 import { addSlackEntry } from "../support/slack.driver.js";
+import { AppDriver } from "../support/app.driver.js";
 
 describe('Add slack entry and verify on BusyBe website', () => {
   it('should add entry via slack and verify on BusyBe website', () => {
-    const slackToken = Cypress.env('SLACK_TOKEN');
+    
     const testMessage = `E2E Rocks! ${new Date().toISOString()}`
 
-    if (!slackToken) {
-      throw new Error("SLACK_TOKEN is not defined. Please set it before running the test.");
-    }
+    addSlackEntry(testMessage)
 
+    AppDriver.visitApp()
+    
+    AppDriver.verifyEntryDisplayed(testMessage)
 
-    addSlackEntry(slackToken, testMessage).then((response) => {
-      expect(response.status).to.eq(200);
-      cy.log("Entry successfully added via Slack API");
-    });
-
-    cy.visit('https://app.busybeapp.com')
-
-    // Wait for the entry to appear on the page
-    cy.contains(testMessage, { timeout: 10000 })
-      .should("be.visible")
-      .log("Entry is displayed on the BusyBe website");
     });
   })
